ed1b0e701c25bfdcd94012b34c7102e33030d4c3
gibbssonfarias30 <gibbfarc17@gmail.com>
2023-04-22 20:17:54 -0500

    project created for the use of feign client

---
+++ src/main/java/com/backfcdev/useopenfeign/UseOpenfeignApplication.java
@@ -0,0 +1,13 @@
+package com.backfcdev.useopenfeign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UseOpenfeignApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UseOpenfeignApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backfcdev/useopenfeign/UseOpenfeignApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backfcdev.useopenfeign;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UseOpenfeignApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
