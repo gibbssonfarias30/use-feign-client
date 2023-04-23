3329a8639f9b55225ec3d35e2a11f0b2d3c13f6d
gibbssonfarias30 <gibbfarc17@gmail.com>
2023-04-22 20:34:06 -0500

    added @EnableFeignClients annotation in project main

--- src/main/java/com/backfcdev/useopenfeign/UseOpenfeignApplication.java
+++ src/main/java/com/backfcdev/useopenfeign/UseOpenfeignApplication.java
@@ -2,8 +2,10 @@ package com.backfcdev.useopenfeign;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients // We notify spring that we are going to use openfeign client in the context or service
 public class UseOpenfeignApplication {
 
 	public static void main(String[] args) {
